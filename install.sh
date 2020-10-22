47357a3f3a08eb2709a7769dabb79855681495d7
Puttipong Wongrak <puttipong.wongrak@gmail.com>
2020-10-22 17:52:10 +0700

    fix filename invalid

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-cp .terraform-aliases ~/
+#!/bin/bash
+cp .terraform_aliases ~/
 echo "[ -f ~/.terraform_aliases ] && source ~/.terraform_aliases" >> ~/.bashrc
-echo "[ -f ~/.terraform_aliases ] && source ~/.terraform_aliases" >> ~/.zshrc
+#echo "[ -f ~/.terraform_aliases ] && source ~/.terraform_aliases" >> ~/.zshrc
